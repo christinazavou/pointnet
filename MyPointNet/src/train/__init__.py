40d425540ca0f9a6652075f6ca8c465193dfa2dc
christinazavou <czavou01@gmail.com>
2020-07-17 17:25:51 +0300

    looking on batch normalization

--- MyPointNet/src/train/__init__.py
+++ MyPointNet/src/train/__init__.py
@@ -1,4 +1,4 @@
-from src.io.config import parse_configuration
-
-
-parse_configuration(argv=['gpu', '4'])
+# from src.io.config import parse_configuration
+#
+#
+# parse_configuration(argv=['gpu', '4'])
